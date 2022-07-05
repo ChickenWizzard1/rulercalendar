da54413bdac9d5a0bad7a774e9ba73f43525b5da
ChickenWizzard1 <102372115+ChickenWizzard1@users.noreply.github.com>
2022-07-05 16:50:37 +0200

    Update index.py

--- index.py
+++ index.py
@@ -16,20 +16,49 @@
             rc_minutes_floor = Math.floor(rc_minutes)
 
             rc_seconds = (rc_minutes - rc_minutes_floor) * 60
-            rc_seconds_floor = Math.floor(rc_seconds)"""
-
+            rc_seconds_floor = Math.floor(rc_seconds)
+import math
 import datetime
 from datetime import timedelta
 day_length_milliseconds = 24 * 60 * 60 * 1000
-now = datetime.datetime.now()
-epoch = [2022, 1, 1, 0, 0, 0,]
- #days_since_epoch = now - epoch
-#definine time in this moment.
-now = [now.year, now.day, now.month, now.hour, now.minute, now.second]
-print (day_length_milliseconds)
-# subtract daytime (not done)  
-d = datetime.datetime.today() - timedelta(days=2)
-#just for me so I can see what d is a lot easier.
+now = datetime.datetime.today()
+epoch = datetime.datetime(2022, 1, 1, 0, 0, 0)
+d = (now - epoch)
+ #just for me so I can see what d is a lot easier.
 print("d is:")
 print(d)
 print(now)
+days_since_epoch= d / day_length_milliseconds
+print (day_length_milliseconds)
+print(days_since_epoch)
+rc_years = days_since_epoch / 30.4375
+print(rc_years)"""
+#import pandas as pd
+
+import datetime
+import math
+def time_to_int(dateobj):
+    total = int(dateobj.strftime('%S'))
+    total += int(dateobj.strftime('%M')) * 60
+    total += int(dateobj.strftime('%H')) * 60 * 60
+    total += (int(dateobj.strftime('%j')) - 1) * 60 * 60 * 24
+    total += (int(dateobj.strftime('%Y')) - 1970) * 60 * 60 * 24 * 365
+    return total
+
+today = datetime.date.today()
+d1 = datetime.date(year=2022, month=1, day=1)
+d1 = time_to_int(d1)
+today  = time_to_int(today)
+d2 =  today-d1
+
+day_length_milliseconds = 24 * 60 * 60 * 1000
+days_since_epoch = d2 / day_length_milliseconds
+days_since_epoch = days_since_epoch * 1000
+
+print(days_since_epoch)
+rc_years = days_since_epoch / 30.4375
+rc_years_floor = math.floor(rc_years)
+rc_months = (rc_years - rc_years_floor) * 12
+rc_months_floor = math.floor(rc_months)
+print(rc_years, rc_months)
+print(rc_years_floor, rc_months_floor)
